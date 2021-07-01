bc4deaf695cd02f192444fa64328e6e5f31bf764
WOWDeveloperX <wowdeveloperx@gmail.com>
2021-07-01 16:24:38 +0300

    master

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:rickandmorty/dataLoader.dart';
-import 'package:http/http.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -53,18 +52,25 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  
-  
-  
   @override
   void initState() {
     super.initState();
     loadData();
   }
 
-
   @override
   Widget build(BuildContext context) {
+    Widget content;
+    final List<Person>? currentPersons = persons;
+    final Exception? exception = error;
+
+    if (currentPersons != null)
+      content = personsList(context, currentPersons);
+    else if (exception != null)
+      content = exceptionStub(context, exception);
+    else
+      content = loader(context);
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -88,41 +94,39 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Widget personsList(BuildContext context, List<Person> persons) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'persons list leght is ${persons.length}',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ],
+    );
+  }
 
-Widget personsList(BuildContext context, List<Person> persons) {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      Text(
-        'persons list leght is ${persons.length}',
-        style: Theme.of(context).textTheme.headline4,
-      ),
-    ],
-  );
-}
-Widget loader (BuildContext context, List<Person> persons) {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      Text(
-        'load data...',
-        style: Theme.of(context).textTheme.headline4,
-      ),
-    ],
-  );
-}
+  Widget loader(BuildContext context, List<Person> persons) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'load data...',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ],
+    );
+  }
 
-
-Widget exceptionStub (BuildContext context, Exception exception) {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      Text(
-        'OOOPS ERROR IS ${exception.toString()}',
-        style: Theme.of(context).textTheme.headline4,
-      ),
-    ],
-  );
-}
-
+  Widget exceptionStub(BuildContext context, Exception exception) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'OOOPS ERROR IS ${exception.toString()}',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ],
+    );
+  }
 }
